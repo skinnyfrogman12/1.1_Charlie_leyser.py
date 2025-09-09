ae6a238fdd8ecb1272494399fe019e943d5db1ca
skinnyfrogman12 <charliel.23218@obhs.school.nz>
2025-09-09 15:02:58 +1200

    Update 1.1_charlie_leyser.py

--- 1.1_charlie_leyser.py
+++ 1.1_charlie_leyser.py
@@ -7,6 +7,12 @@
 '''
 
 
+#--------------main--------------
+user_name = str(input('Enter your name'))
+age = int(input("Enter your age")) #asks for user name and age
+
+
+
 
 #--------functions---------
 # To check how many pull up user has been doing 
@@ -14,19 +20,27 @@
 # also add all total pull ups that user input and store in the list.
 # loop the question 5 times if no error or no invalid number.
 
+pull_up_result = [] #input will be stored in this list 
 
-#-------libaries--------
-user_name = str(input('Enter your name'))
-age = int(input("Enter your age"))
-pull_up = int(input("How many pull up have you done today"))
-
-pull_up_result=[]
-
+#loop the question for five times
 for i in range(5):
-    result = int(input(f"Enter pull up result"))
-    pull_up_result.append(result)
+    while True:
+        pull_up = int(input("How many pull up have you done today")) #input how many pull-ups user did for today 
+        pull_up_result.append(pull_up) #input variable will go into this list 
 
-    print(f"Hi {user_name}")
-    print(pull_up)
+        if(pull_up >= 50):
+            print("Nice!")
 
-#-------main----------
+        else:
+            print("You can do better ")
+
+        break
+
+    print(f'These are your pull up: {pull_up_result}') # list of all the users results    
+
+average = sum(pull_up_result) / len(pull_up_result) #to find the average and print it 
+print("average pull-ups:", round(average, 2))
+
+best_result = max (pull_up_result) #find the max and print it 
+print(f"Hi {user_name}")
+print (f'Your best result is {best_result}')
